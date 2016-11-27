bedca966c38eacf64a59d683e3d47578a913c29d
yurireis <yrreis@gmail.com>
2016-11-27 12:45:05 -0300

    mensagem de desconexao

--- src/com/company/Player.java
+++ src/com/company/Player.java
@@ -5,6 +5,7 @@ import java.io.DataOutputStream;
 import java.io.EOFException;
 import java.io.IOException;
 import java.net.Socket;
+import java.rmi.server.ExportException;
 
 /**
  * Created by yurireis on 15/11/16.
@@ -33,7 +34,8 @@ public class Player implements Runnable{
 
     @Override
     public void run() {
-        while (true){
+        boolean run = true;
+        while (run){
             try{
                 String received1 = input.readUTF();
                 opponent.output.writeUTF(received1);
@@ -41,18 +43,31 @@ public class Player implements Runnable{
 
                 Thread.sleep(500);
             }
-            catch (Exception e){
+            catch (EOFException e){
+                opponent.notifyDisconnection();
+                System.out.print("Player desconectado");
+                run = false;
+            }
+            catch (Exception e) {
                 e.printStackTrace();
-                //Disconnected
-                break;
+                run = false;
             }
         }
-        Main.games--;
     }
 
     public void notifyConnection(Integer position){
         try {
             output.writeUTF("0-" + position.toString());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void notifyDisconnection(){
+        try {
+            output.writeUTF("5-0");
+            output.close();
+            Main.games--;
         } catch (IOException e) {
             e.printStackTrace();
         }
